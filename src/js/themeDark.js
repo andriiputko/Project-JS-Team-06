d0076ca35606f80e62dc78956ed1bc8e4c5bd168
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-07-10 23:00:12 +0300

    зміни js для темної теми

--- src/js/themeDark.js
+++ src/js/themeDark.js
@@ -1,18 +1,25 @@
-"use strict"
+
+"use strict";
 
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
-  // Додаємо клас "darkmode" до body
-  document.body.classList.add('darkmode');
+  // Додаємо клас "darkmode" до body з плавним ефектом
+  setTimeout(() => {
+    document.body.classList.add('darkmode');
+  }, 300); // Затримка 300 мс (0.3 секунди)
+
   // Оновлюємо значення "darkMode" у localStorage
   localStorage.setItem('darkMode', 'enabled');
 };
 
 const disableDarkMode = () => {
-  // Видаляємо клас "darkmode" з body
-  document.body.classList.remove('darkmode');
+  // Видаляємо клас "darkmode" з body з плавним ефектом
+  setTimeout(() => {
+    document.body.classList.remove('darkmode');
+  }, 300); // Затримка 300 мс (0.3 секунди)
+
   // Оновлюємо значення "darkMode" у localStorage
   localStorage.setItem('darkMode', null);
 };
@@ -25,10 +32,11 @@ if (darkMode === 'enabled') {
 // Додаємо обробник події "click" на кнопку перемикача теми
 darkModeToggle.addEventListener('click', () => {
   darkMode = localStorage.getItem('darkMode');
-  
+
   if (darkMode !== 'enabled') {
     enableDarkMode();
   } else {
     disableDarkMode();
   }
 });
+
