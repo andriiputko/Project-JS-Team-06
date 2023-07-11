065d49083a6531fc2eab7c13251f394990fa46a5
OlgaBezeiko <bezejkoolga@gmail.com>
2023-07-11 20:37:22 +0300

    some fix theme
    Додала зміни в джс, щоб працювала одна із модалок

--- src/js/themeDark.js
+++ src/js/themeDark.js
@@ -1,35 +1,74 @@
 
-"use strict";
+// "use strict";
+
+// let darkMode = localStorage.getItem('darkMode');
+// const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+
+// const enableDarkMode = () => {
+//   // Додаємо клас "darkmode" до body з плавним ефектом
+//   setTimeout(() => {
+//     document.body.classList.add('darkmode');
+//   }, 300); // Затримка 300 мс (0.3 секунди)
+
+//   // Оновлюємо значення "darkMode" у localStorage
+//   localStorage.setItem('darkMode', 'enabled');
+// };
+
+// const disableDarkMode = () => {
+//   // Видаляємо клас "darkmode" з body з плавним ефектом
+//   setTimeout(() => {
+//     document.body.classList.remove('darkmode');
+//   }, 300); // Затримка 300 мс (0.3 секунди)
+
+//   // Оновлюємо значення "darkMode" у localStorage
+//   localStorage.setItem('darkMode', null);
+// };
+
+// // Перевіряємо, чи включений темний режим
+// if (darkMode === 'enabled') {
+//   enableDarkMode();
+// }
+
+// // Додаємо обробник події "click" на кнопку перемикача теми
+// darkModeToggle.addEventListener('click', () => {
+//   darkMode = localStorage.getItem('darkMode');
+
+//   if (darkMode !== 'enabled') {
+//     enableDarkMode();
+//   } else {
+//     disableDarkMode();
+//   }
+// });
+
+'use strict';
 
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const modal = document.querySelector('[data-modal-book]');
 
 const enableDarkMode = () => {
-  // Додаємо клас "darkmode" до body з плавним ефектом
   setTimeout(() => {
     document.body.classList.add('darkmode');
-  }, 300); // Затримка 300 мс (0.3 секунди)
+    modal.classList.add('darkmode'); // Додано клас 'darkmode' до контейнера модального вікна
+  }, 300);
 
-  // Оновлюємо значення "darkMode" у localStorage
   localStorage.setItem('darkMode', 'enabled');
 };
 
 const disableDarkMode = () => {
-  // Видаляємо клас "darkmode" з body з плавним ефектом
   setTimeout(() => {
     document.body.classList.remove('darkmode');
-  }, 300); // Затримка 300 мс (0.3 секунди)
+    modal.classList.remove('darkmode'); // Видалено клас 'darkmode' з контейнера модального вікна
+  }, 300);
 
-  // Оновлюємо значення "darkMode" у localStorage
   localStorage.setItem('darkMode', null);
 };
 
-// Перевіряємо, чи включений темний режим
 if (darkMode === 'enabled') {
   enableDarkMode();
 }
 
-// Додаємо обробник події "click" на кнопку перемикача теми
 darkModeToggle.addEventListener('click', () => {
   darkMode = localStorage.getItem('darkMode');
 
@@ -39,4 +78,3 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode();
   }
 });
-
