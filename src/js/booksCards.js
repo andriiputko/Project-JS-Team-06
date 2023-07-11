46ef6d23f1ca00034500ee73cecb2206b4afd646
Leonid9797 <123494748+Leonid9797@users.noreply.github.com>
2023-07-11 19:27:49 +0200

    updated js book API/ booksCards

--- src/js/booksCards.js
+++ src/js/booksCards.js
@@ -1,11 +1,32 @@
 import booksAPI from './booksAPI.js';
-
-
 import { modalFunc } from './modal-open-close';
-  
 
 const instanceBooksAPI = new booksAPI();
 const booksContainer = document.querySelector('.book-category-lists');
+
+function generateBookListHTML(bookList) {
+  return `
+    <ul class="book-category-list">
+      ${bookList
+        .map(
+          book => `
+            <li class="book-category-list-card">
+                <a class="book-category-hover-effect-container">
+                  <img class="book-category-list-img" data-id="${book._id}" src="${book.book_image}" alt="${book.title}" loading="lazy">
+                  <div class="overlay">
+                    <p class="book-category-hover-effect">QUICK VIEW</p>
+                  </div>
+                </a>
+                <h3 class="book-category-list-category-book-name">${book.title}</h3>
+                <p class="book-category-list-book-author">${book.author}</p>
+              </li>
+          `
+        )
+        .join('')}
+    </ul>
+  `;
+}
+
 export function generateBookCategoryElements(data) {
   const elements = [];
 
@@ -31,12 +52,44 @@ export function generateBookCategoryElements(data) {
           )
           .join('')}
       </ul>
+      <div class="book-list-container"></div>
       <div class="button-container">
         <button class="book-category-btn" data-category="${
           category.list_name
         }" type="button">SEE MORE</button>
       </div>
     `;
+
+    const button = categoryWrapper.querySelector('.book-category-btn');
+    button.addEventListener('click', async () => {
+      const bookListContainer = categoryWrapper.querySelector(
+        '.book-list-container'
+      );
+
+      try {
+        const bookList = await instanceBooksAPI.fetchCategoryBooks(
+          category.list_name
+        );
+
+        if (!!bookList.length) {
+          const existingIds = [];
+          const currentCategory = data.filter(
+            item => item.list_name === category.list_name
+          );
+          currentCategory[0].books.map(item => existingIds.push(item._id));
+
+          const filteredArray = bookList.filter(
+            book => !existingIds.includes(book._id)
+          );
+          if (!filteredArray.length) return;
+
+          const bookListHTML = generateBookListHTML(filteredArray);
+          bookListContainer.innerHTML = bookListHTML;
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    });
 
     elements.push(categoryWrapper);
   }
@@ -53,8 +106,7 @@ export async function showCards() {
       booksContainer.appendChild(element);
     });
 
-modalFunc();
-
+    modalFunc();
   } catch (error) {
     console.error('Error:', error);
   }
