6692b18c6896f78cfcc73de0c2f926412f6dfe08
Makarenko Pavlo <cmertnik1986@gmail.com>
2023-07-12 00:22:33 +0300

    scroll final

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -1,21 +1,55 @@
+function throttle(func, delay) {
+  let timeoutId;
+  let isThrottled = false;
+
+  return function () {
+    if (!isThrottled) {
+      func.apply(this, arguments);
+      isThrottled = true;
+
+      timeoutId = setTimeout(function () {
+        isThrottled = false;
+      }, delay);
+    }
+  };
+}
+
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function () {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(func, delay);
+  };
+}
+
 function scrollToTop() {
-  const currentPosition = window.pageYOffset;
-  if (currentPosition > 0) {
+  if (window.pageYOffset > 0) {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: 'smooth',
     });
   }
 }
 
-window.addEventListener('scroll', function() {
-  const scrollToTopButton = document.getElementById('scrollToTopButton');
-  if (window.pageYOffset > 100) {
-    scrollToTopButton.style.display = 'block';
-  } else {
-    scrollToTopButton.style.display = 'none';
-  }
-});
+window.addEventListener(
+  'scroll',
+  throttle(function () {
+    const scrollToTopButton = document.getElementById('scrollToTopButton');
+    if (window.pageYOffset > 100) {
+      scrollToTopButton.style.display = 'block';
+    } else {
+      scrollToTopButton.style.display = 'none';
+    }
+  }, 200)
+);
 
 const scrollToTopButton = document.getElementById('scrollToTopButton');
-scrollToTopButton.addEventListener('click', scrollToTop);
+
+scrollToTopButton.addEventListener(
+  'click',
+  debounce(function () {
+    scrollToTop();
+  }, 300)
+);
