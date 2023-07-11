82904346c609cf8b8e1fc73ed1e08a1be28fdc42
Makarenko Pavlo <cmertnik1986@gmail.com>
2023-07-12 00:31:09 +0300

    1

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -1,21 +1,69 @@
+// Функция throttle
+function throttle(func, delay) {
+  let timeoutId;
+  let isThrottled = false;
+
+  return function () {
+    if (!isThrottled) {
+      func.apply(this, arguments);
+      isThrottled = true;
+
+      timeoutId = setTimeout(function () {
+        isThrottled = false;
+      }, delay);
+    }
+  };
+}
+
+// Функция debounce
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function () {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(func, delay);
+  };
+}
+
+// Код для прокрутки вверх
 function scrollToTop() {
-  const currentPosition = window.pageYOffset;
-  if (currentPosition > 0) {
+  if (window.pageYOffset > 0) {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: 'smooth',
     });
   }
 }
 
-window.addEventListener('scroll', function() {
+// Функция обновления состояния кнопки "прокрутить вверх"
+function updateScrollButtonVisibility() {
   const scrollToTopButton = document.getElementById('scrollToTopButton');
   if (window.pageYOffset > 100) {
     scrollToTopButton.style.display = 'block';
   } else {
     scrollToTopButton.style.display = 'none';
   }
-});
+}
 
+// Обработчик события скролла с применением throttle
+window.addEventListener(
+  'scroll',
+  throttle(function () {
+    updateScrollButtonVisibility();
+  }, 200)
+);
+
+// Получаем ссылку на кнопку прокрутки вверх
 const scrollToTopButton = document.getElementById('scrollToTopButton');
-scrollToTopButton.addEventListener('click', scrollToTop);
+
+// Обработчик события клика с применением debounce
+scrollToTopButton.addEventListener(
+  'click',
+  debounce(function () {
+    scrollToTop();
+  }, 300)
+);
+
+// Инициализация состояния кнопки при загрузке страницы
+updateScrollButtonVisibility();
