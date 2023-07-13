b4b1cf9a656cb0d0cdd6e25707abdbf8784e3b1d
NEYROSICK <dhatak24@gmail.com>
2023-07-13 13:27:38 +0300

    sign-up-modal
    fixed sign up modal opening
    fixed little css issues

--- src/js/sign-up-modal.js
+++ src/js/sign-up-modal.js
@@ -1,39 +1,35 @@
- const openSignUpBtnEl =  document.querySelector('#signUp-open-btn');
- const signUpModalEl = document.querySelector('#sign-up-modal');
- const signUpModalWindowEl = document.querySelector('#sign-up-modal-window');
- const closeSignUpBtnEl = document.querySelector('#signUp-modal-close-btn');
- 
+const openSignUpBtnEl = document.querySelectorAll('.signUp-open-btn');
+const signUpModalEl = document.querySelector('#sign-up-modal');
+const signUpModalWindowEl = document.querySelector('#sign-up-modal-window');
+const closeSignUpBtnEl = document.querySelector('#signUp-modal-close-btn');
 
+openSignUpBtnEl.forEach(btn => {
+  btn.addEventListener('click', handleClickOnSingUpOpenBtn);
+});
 
- openSignUpBtnEl.addEventListener('click', handleClickOnSingUpOpenBtn);
- closeSignUpBtnEl.addEventListener('click', handleClickOnSingUpCloseBtn);
- signUpModalEl.addEventListener('click', handleClickOnsingUpBackdrop);
+closeSignUpBtnEl.addEventListener('click', handleClickOnSingUpCloseBtn);
+signUpModalEl.addEventListener('click', handleClickOnsingUpBackdrop);
 
+function handleClickOnSingUpOpenBtn() {
+  signUpModalEl.classList.add('open');
+}
 
+function handleClickOnSingUpCloseBtn() {
+  signUpModalEl.classList.remove('open');
+}
 
-
-    function handleClickOnSingUpOpenBtn() {
-        signUpModalEl.classList.add('open');
-       
-      }
-    
-    function handleClickOnSingUpCloseBtn() {
-        signUpModalEl.classList.remove('open');
+window.addEventListener('keydown', evt => {
+  if (signUpModalEl.classList.contains('open')) {
+    if (evt.key === 'Escape') {
+      signUpModalEl.classList.remove('open');
     }
+  }
+});
 
-    window.addEventListener('keydown', evt => {
-        if (signUpModalEl.classList.contains('open')) {
-          if (evt.key === 'Escape') {
-            signUpModalEl.classList.remove('open');
-          }
-        }
-    });
+function handleClickOnsingUpBackdrop(evt) {
+  const target = evt.target;
 
-
-    function handleClickOnsingUpBackdrop(evt) {
-        const target = evt.target;
-      
-        if (target === signUpModalEl) {
-          handleClickOnSingUpCloseBtn();
-        }
-    }
+  if (target === signUpModalEl) {
+    handleClickOnSingUpCloseBtn();
+  }
+}
